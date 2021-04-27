5f8429e6e7cc38f4c4c62bc437de345a71dbcaa2
Abhishek <abhi.9811206369@gmail.com>
2021-04-27 19:44:31 +0530

    update title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Popup Login Demo' });
 });
 
 router.get('/login', function(req, res) {
